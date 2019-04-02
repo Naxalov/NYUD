0702c82fa06f8c9af3729b5e57175bbe22e49a79
Zarif <uzbzarif@gmail.com>
2019-04-02 11:01:41 +0900

    imshow

--- Data Visualization/read.py
+++ Data Visualization/read.py
@@ -1,10 +1,29 @@
 import skimage.io as io
 import numpy as np
+import matplotlib.pyplot as plt
 import h5py
 
 PATH = '../nyu_depth_v2_labeled.mat'
 
 # read mat file
-f = h5py.File(PATH)
-img = f['images'][0]
+FILE = h5py.File(PATH)
+# Discovering keys
+keys = list(FILE.keys())
+print(keys)
+img = FILE['images'][0]
 print(img.shape)
+# reshape
+img_ = np.empty([480, 640, 3])
+img_[:, :, 0] = img[0, :, :].T
+img_[:, :, 1] = img[1, :, :].T
+img_[:, :, 2] = img[2, :, :].T
+
+# imshow
+img__ = img_.astype('float32')
+io.imshow(img__ / 255.0)
+io.show()
+
+# print(img.shape)
+# plt.imshow(img_/255)
+# plt.show()
+# See sample image
